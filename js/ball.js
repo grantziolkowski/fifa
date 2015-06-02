8f13dd45a4ff40c0d80e4d481c2baf5eb2226c80
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-02 15:59:47 -0400

    Separate ball.js file

---
+++ js/ball.js
@@ -0,0 +1,23 @@
+var Ball = function() {
+  posX: 5,
+  posY: 0,
+  shoot: function() {
+    shot_distance = (40 - ball.posX);
+    ball.posX +=  shot_distance;
+    console.log("You are shooting " + shot_distance + " to the right!")
+    if (ball.posY < 10 || ball.posY > 30){
+      console.log("You missed, but you get your ball back. Keep playing!")
+      ball.posX = (player.posX + 5)
+      }
+    else if (ball.posY === goalie.posY){
+      console.log("The goalie saved your shot, but you get your ball back. Keep playing!")
+      ball.posX = (player.posX + 5)
+      }
+    else {
+      console.log("GOAL!!!")
+      player.scored = true
+     }
+     goalie.posY = Math.floor((Math.random()*20)+10)
+     }
+
+};
