b8cbff83cd170ff7616232931b8e5913832aebd6
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-02 16:00:53 -0400

    Separate player.js file

--- js/fifa.js
+++ js/fifa.js
@@ -1,29 +1,3 @@
-var Player = function() {
-  posX: 0,
-  posY: 0,
-  scored: false,
-  move: function(direction) {
-   if(direction === 'right'){
-    player.posX +=10;
-   }
-   else if(direction === 'left'){
-    player.posX -=10;
-  }
-   else if(direction === 'up'){
-    player.posY += 10;
-   }
-   else if(direction === 'down'){
-     player.posY -=10;
-   }
-  console.log("Your position: " + player.posX + ", " + player.posY)
-  ball.posX = (player.posX + 5);
-  ball.posY = (player.posY);
-
-  }
-
-};
-
-
 
 var shot_distance = (40 - ball.posX);
 
