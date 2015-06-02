8d2efc49e59323ed2153f0d1ed1808f30fc51403
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-02 16:27:11 -0400

    Make ball.shoot a prototype, change ball references to this

--- js/ball.js
+++ js/ball.js
@@ -1,25 +1,23 @@
 function Ball() {
-  this.posX: 5,
-  this.posY: 0
+  this.posX = 5;
+  this.posY = 0;
+  this.$ele = $('<div id="#ball"></div>').appendTo($('body'));
 }
 
 Ball.prototype.shoot = function() {
-    shot_distance = (40 - ball.posX);
-    ball.posX +=  shot_distance;
+    this.shot_distance = (40 - ball.posX);
+    this.posX +=  this.shot_distance;
     console.log("You are shooting " + shot_distance + " to the right!")
-    if (ball.posY < 10 || ball.posY > 30){
-      console.log("You missed, but you get your ball back. Keep playing!")
-      ball.posX = (player.posX + 5)
-      }
-    else if (ball.posY === goalie.posY){
-      console.log("The goalie saved your shot, but you get your ball back. Keep playing!")
-      ball.posX = (player.posX + 5)
-      }
-    else {
+    if (this.posY < 10 || this.posY > 30) {
+      console.log("You missed, but you get your this back. Keep playing!")
+      this.posX = (player.posX + 5)
+    } else if (this.posY === goalie.posY) {
+      console.log("The goalie saved your shot, but you get your this back. Keep playing!")
+      this.posX = (player.posX + 5)
+    } else {
       console.log("GOAL!!!")
       player.scored = true
-     }
-     goalie.posY = Math.floor((Math.random()*20)+10)
-     }
+    }
+    goalie.posY = Math.floor((Math.random()*20)+10);
 
 };
