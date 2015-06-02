04e959de8ca355a3a18b55dcf059c2bc04bc89ae
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-02 16:29:46 -0400

    Make player.move a prototype, change player references to this

--- js/fifa.js
+++ js/fifa.js
@@ -1,47 +1,45 @@
 $(document).ready(function() {
   var arena = new Arena();
   var ball = new Ball();
-})
-
-var shot_distance = (40 - ball.posX);
-
-var goalie = {
+  var shot_distance = (40 - ball.posX);
+  var goalie = {
   posX: 35,
   posY: Math.floor((Math.random()*20)+10)
 };
+})
 
 // Driver Test Code:
-console.log("The ball position is: " + ball.posX + ", " + ball.posY);
-player.move('up');
-player.move('right');
-player.move('left');
-player.move('left');
-player.move('down');
-player.move('right');
-player.move('right');
-player.move('right');
-console.log("The ball position is: " + ball.posX + ", " + ball.posY);
-console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
-ball.shoot();
-player.move('up');
-player.move('right');
-player.move('left');
-player.move('left');
-player.move('down');
-player.move('right');
-player.move('left');
-player.move('up');
-player.move('up');
-console.log("The ball position is: " + ball.posX + ", " + ball.posY);
-console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
-ball.shoot();
-player.move('left');
-player.move('left');
-player.move('down');
-player.move('up');
-console.log("The ball position is: " + ball.posX + ", " + ball.posY);
-console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
-ball.shoot();
-console.log("The ball position is: " + ball.posX + ", " + ball.posY);
-console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
-ball.shoot();
+// console.log("The ball position is: " + ball.posX + ", " + ball.posY);
+// player.move('up');
+// player.move('right');
+// player.move('left');
+// player.move('left');
+// player.move('down');
+// player.move('right');
+// player.move('right');
+// player.move('right');
+// console.log("The ball position is: " + ball.posX + ", " + ball.posY);
+// console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
+// ball.shoot();
+// player.move('up');
+// player.move('right');
+// player.move('left');
+// player.move('left');
+// player.move('down');
+// player.move('right');
+// player.move('left');
+// player.move('up');
+// player.move('up');
+// console.log("The ball position is: " + ball.posX + ", " + ball.posY);
+// console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
+// ball.shoot();
+// player.move('left');
+// player.move('left');
+// player.move('down');
+// player.move('up');
+// console.log("The ball position is: " + ball.posX + ", " + ball.posY);
+// console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
+// ball.shoot();
+// console.log("The ball position is: " + ball.posX + ", " + ball.posY);
+// console.log("The goalie position is: " + goalie.posX + ", " + goalie.posY);
+// ball.shoot();
--- js/player.js
+++ js/player.js
@@ -1,22 +1,21 @@
 function Player() {
-  this.posX: 0,
-  this.posY: 0,
-  this.scored: false
+  this.posX = 0;
+  this.posY = 0;
+  this.scored = false;
 }
 
 Player.prototype.move = function(direction) {
   if(direction === 'right'){
-    player.posX +=10;
+    this.posX +=10;
   } else if(direction === 'left'){
-    player.posX -=10;
+    this.posX -=10;
   } else if(direction === 'up'){
-     player.posY += 10;
+     this.posY += 10;
   } else if(direction === 'down'){
-     player.posY -=10;
+     this.posY -=10;
   }
-  console.log("Your position: " + player.posX + ", " + player.posY)
-  ball.posX = (player.posX + 5);
-  ball.posY = (player.posY);
-  }
+  console.log("Your position: " + this.posX + ", " + this.posY)
+  ball.posX = (this.posX + 5);
+  ball.posY = (this.posY);
 
 };
