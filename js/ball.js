041fbcfbca0415f04594dc1af4ff77e35ad5fade
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-03 16:19:20 -0400

    Add Goalie dimensions to correctly block shot

--- js/ball.js
+++ js/ball.js
@@ -15,30 +15,35 @@ Ball.prototype.setPos = function() {
 }
 
 Ball.prototype.move = function(x, y) {
+  this.$ele.addClass('move')
   this.posX = x + 48;
   this.posY = y + 25;
   this.setPos();
 }
 
 Ball.prototype.shoot = function() {
-    this.shotDistance = 730 - this.posX;
-    this.posX +=  this.shotDistance;
-    console.log("You are shooting " + this.shotDistance + " to the right!")
+    var checkGoalieTop = this.goalie.posY + 10
+    var checkGoalieBottom = this.goalie.posY + 30
+    var shotDistance = 730 - this.posX;
+    this.posX +=  shotDistance;
+    console.log("You are shooting " + shotDistance + " to the right!")
     this.setPos();
-    if (this.posY < 250 || this.posY > 290) {
+    if (this.posY < 265 || this.posY > 325) {
       console.log("You missed, but you get your ball back. Keep playing!")
       var that = this;
       setTimeout(function(){
       that.punt();
     }, 1000);
-    } else if (this.posY === this.goalie.posY) {
+    } else if (this.posY > checkGoalieTop &&
+      this.posY < checkGoalieBottom) {
       console.log("The goalie saved your shot, but you get your ball back. Keep playing!")
       this.posX -= 75
     } else {
       console.log("GOAL!!!")
       saved = false;
     }
-    this.goalie.posY = Math.floor((Math.random()*20)+10);
+    console.log("ball", this.posY)
+    console.log("top", checkGoalieTop, "bottom", checkGoalieBottom)
 };
 
 Ball.prototype.punt = function() {
